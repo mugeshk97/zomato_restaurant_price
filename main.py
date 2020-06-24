bc5a4da73808ba4619c8ce83dc7aeabd4b57d518
Mugesh Kannan <mugeshk1171@gmail.com>
2020-06-24 14:47:53 +0530

    updated the code with prediction

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from ebrain_train.dbops import Insertion
 from ebrain_train.dataprocess import Preprocess
 from ebrain_train.model import Model_Selector
 from sklearn.model_selection import train_test_split
+from ebrain_train.Logger import Log
+from ebrain_predict.prediction_validation import Validation
+from ebrain_predict.prediction_cleaner import Prediction_Filter
+import pickle
 
 
 class Train:
@@ -28,3 +32,23 @@ class Train:
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
         model = self.model.best_model(X_train, X_test, y_train, y_test)
         return model
+
+
+class Prediction:
+    def __init__(self):
+        self.logger = Log()
+        self.a = Validation()
+        self.b = Prediction_Filter()
+        self.c = Preprocess()
+        with open('models/model-0.8266616494366493.pkl', 'rb') as file:
+            self.model = pickle.load(file)
+
+    def predict(self, filename):
+        self.a.validate(filename)
+        self.data = self.b.transform(filename)
+        self.data = self.c.rating(self.data)
+        self.data = self.c.table(self.data)
+        self.X, self.y = self.c.split(self.data)
+        self.X = self.c.encoder(self.X)
+        prediction = self.model.predict(self.X)
+        return prediction
